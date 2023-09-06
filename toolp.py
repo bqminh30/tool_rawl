306c541db76b62db5c146d3676c89323880a3ff3
bqminh30 <bqminh30@gmail.com>
2023-09-06 18:14:26 +0700

    done

--- toolp.py
+++ toolp.py
@@ -8,23 +8,26 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 # Đường dẫn đến file Excel
-excel_file_path = './hieu2.xlsx'
+excel_file_path = './hieuha.xlsx'
 chrome_driver_path = 'https://www.google.com/'
 
 # Mở file Excel
 workbook = openpyxl.load_workbook(excel_file_path)
 sheet = workbook.active
 
-
-count = 2
+exceptions_list = []
+count = 1
+# df = pd.read_csv('./Book1.xlsx')
+# print(df.shape[1])
 # Lặp qua từng hàng trong dữ liệu Excel và nhập thông tin đăng ký
-for row in sheet.iter_rows(count, values_only=True):  # Bắt đầu từ hàng thứ 2 (hàng đầu tiên chứa tiêu đề)
+for row in sheet.iter_rows(min_row=2, values_only=True):  # Bắt đầu từ hàng thứ 2 (hàng đầu tiên chứa tiêu đề)
     username, email, password, phone, code = row[:5]
     # Khởi tạo trình duyệt web (ví dụ: Chrome)
     driver = webdriver.Chrome()
     # Điều hướng đến trang đăng ký
     driver.get('https://metahome.digital/sign-up')
-    
+    count = count+1
+    time.sleep(10)
     # Tìm các phần tử input và nhập thông tin tương ứng
     driver.find_element(By.CSS_SELECTOR, '#signUp .box form input[name="email"]').send_keys(email)
     driver.find_element(By.CSS_SELECTOR, '#signUp .box form input[name="password"]').send_keys(password)
@@ -41,7 +44,8 @@ for row in sheet.iter_rows(count, values_only=True):  # Bắt đầu từ hàng 
     time.sleep(1)
     #Thực hiện đăng ký
     driver.find_element(By.CSS_SELECTOR, "#signUp .box form button.btn_signup").click()
-    time.sleep(3)
+    time.sleep(5)
+
     try:
         element_to_click  = driver.find_element(By.CSS_SELECTOR, '.btn_wait_kr')
         # Điều hướng đến trang đăng nhập 
@@ -52,7 +56,7 @@ for row in sheet.iter_rows(count, values_only=True):  # Bắt đầu từ hàng 
         # # Thực hiện đăng nhập
         driver.find_element(By.CSS_SELECTOR, "#logIn .box form button[type=submit]").click()
 
-        time.sleep(6)
+        time.sleep(5)
         driver.get('https://metahome.digital/mypage')
         wait = WebDriverWait(driver, 10)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.btn_add')))
@@ -68,9 +72,11 @@ for row in sheet.iter_rows(count, values_only=True):  # Bắt đầu từ hàng 
             time.sleep(2)
             driver.find_element(By.XPATH, ".v-card-title button[type='button']").click()
         except:
-            count+1
+            print(count)
     except:
-        count+1
+        exceptions_list.append(str(count))
+        with open("exceptions.txt", "a") as file:
+            file.write(str(exceptions_list) + "\n")  # Ghi lỗi vào tệp văn bản
+    # Kết thúc
+    driver.quit()
 
-# Kết thúc phiên làm việc
-driver.quit()
